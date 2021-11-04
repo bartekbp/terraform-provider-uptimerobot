c24c38a66edd42abbf98635e096327ca22c0ee05
Mohammed Naser <mnaser@vexxhost.com>
2021-11-04 14:17:02 -0400

    Fix paths

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-	"github.com/vexxhost/terraform-provider-uptimerobot/uptimerobot"
+	"github.com/vexxhost/terraform-provider-uptimerobot/internal/provider"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return uptimerobot.Provider()
+			return provider.Provider()
 		},
 	})
 }
